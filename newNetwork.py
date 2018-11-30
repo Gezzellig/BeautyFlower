f944895c3139f17ecea76b56c19affef1ec00cf1
C. Roest <s2682192@fwn-bborg-edu-74-142.fmnsedu.rug.nl>
2018-11-30 15:41:53 +0100

    made and documented DCU factor

--- newNetwork.py
+++ newNetwork.py
@@ -28,6 +28,33 @@ class superGAN:
         # Build the networks
         self.build_generator()
 
+    def dcu_factor (self, layer_input, filters):
+        """Single layer in the dense blocks
+        """
+        layer = layer_input
+
+        # Batch Normalization:
+        # Normalize the activations of the previous layer at each batch.
+        # I.e. applies a transformation that maintains the mean activation close 
+        # to 0 and the activation standard deviation close to 1.
+        layer = BatchNormalization(momentum=0.8)(layer)
+
+        # ReLu:
+        # Output shape is the same as the input shape.
+        # With default values, it returns element-wise max(x, 0).
+        layer = Activation('relu')(layer)
+
+        # Convolution layer:
+        # Output shape is the same as the shape of the original input with arguments:
+        # strides = 1, and padding = 'same'
+        layer = Conv2D(filters, kernel_size=3, strides=1, padding='same')(layer)
+
+        return layer
+
     def build_generator(self):
         # Input layer with the shape of the low-res images
         input_layer = Input(shape=self.lr_shape)
+
+
+# Notes: 
+# Padding: "same" results in padding the input such that the output has the same length as the original input.
