73f92cd2f9b2698e89fef73061734558661a577f
C. Roest <s2682192@fwn-bborg-edu-74-153.fmnsedu.rug.nl>
2018-12-14 13:35:17 +0100

    lowered epochs nd batchsize

--- maindcu.py
+++ maindcu.py
@@ -10,10 +10,10 @@ print("discriminator summary:")
 gan.discriminator.summary()
 
 # Amount of times the whole dataset is trained
-EPOCHS       = 100
+EPOCHS       = 1
 INPUT_FOLDER = "data/learnset"
 
-for epoch in tqdm(range(EPOCHS)):
-    for batch_idx in range (20):
-        hr_images, lr_images, bicubic = load_preload_images_batch(INPUT_FOLDER, batch_size=50, batch_number=batch_idx)
+for epoch in range(EPOCHS):
+    for batch_idx in tqdm(range (20)):
+        hr_images, lr_images, bicubic = load_preload_images_batch(INPUT_FOLDER, batch_size=1, batch_number=batch_idx)
         gan.train(lr_images, hr_images)
