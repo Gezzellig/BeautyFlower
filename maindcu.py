db1091fe884a8e821664655aa5e0468730729d9e
Kweu <you@example.com>
2019-01-06 16:37:43 +0100

    maindcu prediction, non blocking

--- maindcu.py
+++ maindcu.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from preload import load_preload_images_batch
 import matplotlib.pyplot as plt
+import time
 
 print_summaries = False
 
@@ -27,14 +28,29 @@ if print_summaries:
 	gan.combined_model.summary()
 
 
-#gan.load_weights('dcu_test1')
+gan.load_weights('dcu_test1')
 
+
+t = time.time()
 for epoch in range(EPOCHS):
-    print( "epoch " + str(epoch) )
-    for batch_idx in tqdm(range (20)):
-        hr_images, lr_images, bicubic = load_preload_images_batch(INPUT_FOLDER, batch_size=batch_size, batch_number=batch_idx)
-        gan.train(bicubic, hr_images, batch_size)
+	print( "epoch " + str(epoch) )
+	for batch_idx in tqdm(range (20)):
+		hr_images, lr_images, bicubic = load_preload_images_batch(INPUT_FOLDER, batch_size=batch_size, batch_number=batch_idx)
+		gan.train(bicubic, hr_images, batch_size)
 
+	if epoch % 25 == 0:
+		hr_images, lr_images, bicubic = load_preload_images_batch(INPUT_FOLDER, batch_size=1, batch_number=1)
 
-gan.store_weights('dcu_test1')
+		predicted = gan.generator.predict(bicubic)
+		plt.figure(1)
+		plt.imshow(bicubic[0])
+		plt.show(block=False)
+		plt.pause(0.01)
 
+		plt.figure(2)
+		plt.imshow(predicted[0])
+		plt.show(block=False)
+		plt.pause(0.01)
+		gan.store_weights('dcu_test1')
+
+print ("Finished with time: " + str(time.time() - t))
