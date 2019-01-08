97b58c16805ec7e207c8bb37d437873f3e759b0b
Q.Y.van.Lohuizen@student.rug.nl <s2701782@peregrine.hpc.rug.nl>
2019-01-08 19:20:13 +0100

    peregrine commit

--- maindcu.py
+++ maindcu.py
@@ -5,7 +5,7 @@ print_summaries = True
 
 #PARAMETERS:
 # Epoch = Amount of times the whole dataset is trained
-EPOCHS          = 1
+EPOCHS          = 20
 INPUT_FOLDER    = "data/learnset"
 batch_size 		= 25
 AMOUNT_BATCHES  = 5000 / batch_size
@@ -24,13 +24,13 @@ if print_summaries:
 	print("combined MODELS")
 	gan.combined_model.summary()
 
-gan.load_weights('dcu_test1')
+#gan.load_weights('dcu_test1')
 
-# for epoch in range(EPOCHS):
-# 	print( "epoch " + str(epoch) )
-# 	for batch_idx in range (AMOUNT_BATCHES):
-# 		hr_images, lr_images, bicubic = load_preload_images_batch(INPUT_FOLDER, batch_size=batch_size, batch_number=batch_idx)
-# 		gan.train(bicubic, hr_images, batch_size)
+for epoch in range(EPOCHS):
+	print( "epoch " + str(epoch) )
+	for batch_idx in range (int(AMOUNT_BATCHES)):
+		hr_images, lr_images, bicubic = load_preload_images_batch(INPUT_FOLDER, batch_size=batch_size, batch_number=batch_idx)
+		gan.train(bicubic, hr_images, batch_size)
 
-# 	if epoch % 250 == 0:
+#	if epoch % 250 == 0:
 # 		gan.store_weights('dcu_test1')
