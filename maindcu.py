b407aa42fc18971a0bebd7f561709b86591ca74d
Quintin1995 <Quintin1995>
2019-01-12 14:00:42 +0100

    input args

--- maindcu.py
+++ maindcu.py
@@ -5,18 +5,28 @@ import sys
 import time
 import os
 
+print ('Argument List:', str(sys.argv))
+
+if len (sys.argv) == 4:
+	EPOCHS = int(sys.argv[1])
+	SAVE_INTERVAL = int(sys.argv[2])
+	batch_size = int(sys.argv[3])
+else:
+	print ("""Incorrect number of input arguments, please give input arguments in the following format:\n 
+		python3 maindcu.py EPOCHS SAVE_INTERVAL BATCH_SIZE""")
+	exit()
+
 print_summaries = True
 
-#PARAMETERS:
-# Epoch = Amount of times the whole dataset is trained
-EPOCHS          = 5
-INPUT_FOLDER    = "data/learnset"
-batch_size 	= 2
-AMOUNT_BATCHES  = 10 / batch_size
+
 
 print("Building the gan")
 
 gan = network.BeautyFlower()
+
+#PARAMETERS:
+INPUT_FOLDER    = "data/learnset"
+AMOUNT_BATCHES  = 5000 / batch_size
 
 if print_summaries:
 	print ("epochs: " + str(EPOCHS))
@@ -46,7 +56,7 @@ for epoch in range(EPOCHS):
 
 
 
-	if epoch % 1 == 0:
+	if epoch % SAVE_INTERVAL == 0:
 		filename = "{}/gan{}e{}".format(foldername, time.strftime("%d_%b_%Y_%H:%M:%S", time.gmtime()), epoch)
 		gan.store_weights(filename)
 		print("stored: {}".format(filename))
